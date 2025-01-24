458c1fb104991a74f25a2736ad04743545c82846
sangwook16074 <qazws78941@gmail.com>
2025-01-24 17:58:46 +0900

    전체 조회 기능 제작 완료

--- src/main/kotlin/com/example/yum_recipe_server/recipe/entity/Recipe.kt
+++ src/main/kotlin/com/example/yum_recipe_server/recipe/entity/Recipe.kt
@@ -1,5 +1,6 @@
 package com.example.yum_recipe_server.recipe.entity
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
@@ -8,7 +9,6 @@ import org.springframework.data.elasticsearch.annotations.FieldType
 @Document(indexName = "recipes")
 data class Recipe(
     @Id
-    @Field(type = FieldType.Keyword)
     val id : String,
 
     @Field(type = FieldType.Text)
@@ -17,18 +17,37 @@ data class Recipe(
     @Field(type = FieldType.Text)
     val description : String,
 
-    @Field(type = FieldType.Text)
-    val amount : String,
+    @Field(type = FieldType.Keyword)
+    val amount : String?,
 
-    @Field(type = FieldType.Text)
-    val cookTime : String,
+    @JsonProperty("cook_time")
+    @Field(type = FieldType.Keyword)
+    val cookTime : String?,
 
+    @JsonProperty("image_url")
     @Field(type = FieldType.Text)
-    val imageUrl : String,
+    val imageUrl : String?,
+
+    @Field(type = FieldType.Nested)
+    val ingredients : List<Ingredient>,
+
+    @Field(type = FieldType.Nested)
+    val steps: List<Step>,
 )
 
 data class Ingredient(
+    @Field(type = FieldType.Text)
     val name : String,
 
-    val ea : String,
+    @Field(type = FieldType.Text)
+    val ea : String?,
+)
+
+data class Step(
+    @Field(type = FieldType.Text)
+    val text : String,
+
+    @JsonProperty("image_url")
+    @Field(type = FieldType.Text)
+    val imageUrl : String?,
 )
--- src/main/kotlin/com/example/yum_recipe_server/recipe/repository/RecipeRepository.kt
+++ src/main/kotlin/com/example/yum_recipe_server/recipe/repository/RecipeRepository.kt
@@ -1,8 +1,19 @@
 package com.example.yum_recipe_server.recipe.repository
 
 import com.example.yum_recipe_server.recipe.entity.Recipe
+import org.springframework.data.elasticsearch.annotations.Query
 import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository
 
 interface RecipeRepository: ReactiveElasticsearchRepository<Recipe, String> {
-//    suspend fun findAllRecipes() : List<Recipe>
+    /**
+     * 전체 레시피 조회 쿼리
+     */
+    @Query("""
+        {
+            "query":{
+                "match_all":{}                
+            }
+        }
+    """)
+    suspend fun findAllRecipes() : List<Recipe>
 }
