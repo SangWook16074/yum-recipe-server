8a39621b970863d82db0bd638ed92415c0039ea2
sangwook16074 <qazws78941@gmail.com>
2025-02-17 23:13:26 +0900

    레시피 레포지토리에서 불필요한 주석 제거

--- src/main/kotlin/com/example/yum_recipe_server/recipe/repository/RecipeRepository.kt
+++ src/main/kotlin/com/example/yum_recipe_server/recipe/repository/RecipeRepository.kt
@@ -2,7 +2,6 @@ package com.example.yum_recipe_server.recipe.repository
 
 import com.example.yum_recipe_server.recipe.entity.Recipe
 import org.springframework.data.domain.Pageable
-import org.springframework.data.elasticsearch.annotations.Query
 import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository
 import reactor.core.publisher.Flux
 
@@ -11,7 +10,5 @@ interface RecipeRepository: ReactiveElasticsearchRepository<Recipe, String> {
     /**
      * 전체 레시피 페이지 네이션 조회 쿼리
      */
-//    @Query("{\"from\": 0, \"size\": 20, \"query\": {\"match_all\": {}}}")
-//    suspend fun findAll(page: Int, size : Int): Flux<Recipe>
     fun findAllBy(page: Pageable) : Flux<Recipe>
 }
